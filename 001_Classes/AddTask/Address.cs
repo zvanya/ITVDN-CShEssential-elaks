655252b3eea55ec5d12ce3c0ea6c2ea1a5ba7d1a
zvanya <zvan@ukr.net>
2016-05-17 15:09:10 +0300

    1st commit

---
+++ 001_Classes/AddTask/Address.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+  class Address
+  {
+    string index, country, city, street, house, apartment;
+
+    public string Index
+    {
+      set { index = value; }
+      get { return index; }
+    }
+
+    public string Country
+    {
+      set { country = value; }
+      get { return country; }
+    }
+    public string City
+    {
+      set { city = value; }
+      get { return city; }
+    }
+    public string Street
+    {
+      set { street = value; }
+      get { return street; }
+    }
+
+  }
+}
---
+++ 001_Classes/AddTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Address a = new Address();
+
+      a.Index = "61000";
+      a.City = "Kharkov";
+
+      Console.WriteLine("a.Index = {0}", a.Index);
+
+      Console.ReadKey();
+    }
+  }
+}
