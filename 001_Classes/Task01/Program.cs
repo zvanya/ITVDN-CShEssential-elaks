8b12cfd6531c887c3c02eb439620b1d0a5f9788a
zvanya <zhakin.v@gmail.com>
2016-05-17 16:54:12 +0300

    Task01 complete

--- 001_Classes/Task01/Program.cs
+++ 001_Classes/Task01/Program.cs
@@ -10,7 +10,40 @@ namespace Task01
   {
     static void Main(string[] args)
     {
+      double side1 = 0, side2 = 0;
 
+      Console.Write("Введите длины сторон прямоугольника.\nСторона 1: ");
+      try
+      {
+        side1 = double.Parse(Console.ReadLine());
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("Некорректное значение. Сторона 1: {0}", side1);
+        side1 = 0;
+        Console.WriteLine("Сторона 1: {0}", side1);
+      }
+
+      Console.Write("Сторона 2: ");
+      try
+      {
+        side2 = double.Parse(Console.ReadLine());
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("Некорректное значение. Сторона 2: {0}", side2);
+        side2 = 0;
+        Console.WriteLine("Сторона 2: {0}", side2);
+      }
+
+      Console.WriteLine(new string('-',50));
+
+      Rectangle rec1 = new Rectangle(side1, side2);
+
+      Console.WriteLine("Perimeter = {0}", rec1.Perimeter);
+      Console.WriteLine("Area = {0}", rec1.Area);
+
+      Console.ReadKey();
     }
   }
 }
---
+++ 001_Classes/Task01/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+  class Rectangle
+  {
+    double side1, side2;
+
+    public Rectangle(double side1, double side2)
+    {
+      this.side1 = side1;
+      this.side2 = side2;
+    }
+
+    double AreaCalculator()
+    {
+      return side1 * side2;
+    }
+
+    double PerimeterCalculator()
+    {
+      return (side1 + side2) * 2;
+    }
+
+    public double Area
+    {
+      get
+      {
+        //TODO: проверка корректного ввода side1 и side2 > 0
+        return AreaCalculator();
+      }
+    }
+
+    public double Perimeter
+    {
+      get
+      {
+        //TODO: проверка корректного ввода side1 и side2 > 0
+        return PerimeterCalculator();
+      }
+    }
+  }
+}
