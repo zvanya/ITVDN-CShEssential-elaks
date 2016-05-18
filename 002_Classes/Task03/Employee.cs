c516c55e41931e93ffbd64c8cea1496879627df9
zvanya <zhakin.v@gmail.com>
2016-05-18 17:46:28 +0300

    002 Task03 Complete

--- 002_Classes/Task03/Employee.cs
+++ 002_Classes/Task03/Employee.cs
@@ -8,17 +8,78 @@ namespace Task03
 {
   class Employee
   {
+    public enum PositionList { pos1, pos2, pos3 };
+
     string name = null;
     string surname = null;
-    string position = null;
-    double pay = 0;
+    PositionList position;
     int experience = 0;
-    double tax = 0;
+    double taxKoef = 0.1;
+    double expKoef = 1.2;
 
     public Employee(string name, string surname)
     {
       this.name = name;
       this.surname = surname;
+    }
+
+    public PositionList Position
+    {
+      get { return position; }
+      set
+      {
+        position = value;
+      }
+    }
+
+    public string Name
+    { get { return name; } }
+
+    public string Surname
+    { get { return surname; } }
+
+    public int Experience
+    {
+      get { return experience; }
+      set { experience = value; }
+    }
+
+    double PayCalculate()
+    {
+      switch (Position)
+      {
+        case PositionList.pos1:
+          return expKoef * Experience * 1000;
+        case PositionList.pos2:
+          return expKoef * Experience * 500;
+        case PositionList.pos3:
+          return expKoef * Experience * 100;
+      }
+      return -1;
+    }
+
+    public double Pay
+    {
+      get { return PayCalculate(); }
+    }
+
+    public double Tax
+    {
+      get { return TaxCalculate(); }
+    }
+
+    double TaxCalculate()
+    {
+      switch (Position)
+      {
+        case PositionList.pos1:
+          return Pay * taxKoef;
+        case PositionList.pos2:
+          return Pay * taxKoef;
+        case PositionList.pos3:
+          return Pay * taxKoef;
+      }
+      return -1;
     }
   }
 }
--- 002_Classes/Task03/Program.cs
+++ 002_Classes/Task03/Program.cs
@@ -10,6 +10,22 @@ namespace Task03
   {
     static void Main(string[] args)
     {
+
+      Employee emp1 = new Employee("name1", "surname1");
+      emp1.Position = Employee.PositionList.pos3;
+      emp1.Experience = 2;
+
+      Console.WriteLine("Ставка pos1 = 1000");
+      Console.WriteLine("Ставка pos2 = 500");
+      Console.WriteLine("Ставка pos3 = 100");
+      Console.WriteLine(new string('-', 50));
+      Console.WriteLine("ЗП = Ставка * 1.2 * Стаж");
+      Console.WriteLine("Налог = ЗП * 0.1");
+      Console.WriteLine(new string('-',50));
+
+      Console.WriteLine("Имя: {0}\nФамилия: {1}\nДолжность :{2}\nСтаж: {3}\nЗарплата: {4}\nНалоговый сбор: {5}", emp1.Name, emp1.Surname, emp1.Position, emp1.Experience, emp1.Pay, emp1.Tax);
+
+      Console.ReadKey();
     }
   }
 }
