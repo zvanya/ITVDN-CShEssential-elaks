93c642c463914ff0f3547f46180d5088329acfdc
zvanya <zhakin.v@gmail.com>
2016-05-18 14:59:49 +0300

    Task02 complete

---
+++ 002_Classes/Task02/Converter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+  class Converter
+  {
+    public enum Currency { usd, eur, rub };
+
+    double usd = 0;
+    double eur = 0;
+    double rub = 0;
+
+    public Converter(double usd, double eur, double rub)
+    {
+      this.usd = usd;
+      this.eur = eur;
+      this.rub = rub;
+    }
+
+    public double UahTo(double uah, Currency cur)
+    {
+      switch (cur)
+      {
+        case Currency.usd:
+          return uah / usd;
+        case Currency.eur:
+          return uah / eur;
+        case Currency.rub:
+          return uah / rub;
+      }
+      return -1;
+    }
+
+    public double ToUah(double curVal, Currency cur)
+    {
+      switch (cur)
+      {
+        case Currency.usd:
+          return curVal * usd;
+        case Currency.eur:
+          return curVal * eur;
+        case Currency.rub:
+          return curVal * rub;
+      }
+      return -1;
+    }
+
+  }
+}
---
+++ 002_Classes/Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double val = 0;
+
+      Converter c = new Converter(26, 27, 2.5);
+      
+      val = 1;
+      Console.WriteLine("{0} usd - {1} uah", val, c.ToUah(val, Converter.Currency.usd));
+
+      val = 26;
+      Console.WriteLine("{0} uah - {1} usd", val, c.UahTo(val, Converter.Currency.usd));
+
+      Console.ReadKey();
+    }
+  }
+}
