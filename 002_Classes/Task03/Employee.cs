8e10dd5ca70a92b4e7081418cec8b2f8f74f7ba2
zvanya <zhakin.v@gmail.com>
2016-05-18 16:16:27 +0300

    Task03 added

---
+++ 002_Classes/Task03/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+  class Employee
+  {
+    string name = null;
+    string surname = null;
+    string position = null;
+    double pay = 0;
+    int experience = 0;
+    double tax = 0;
+
+    public Employee(string name, string surname)
+    {
+      this.name = name;
+      this.surname = surname;
+    }
+  }
+}
