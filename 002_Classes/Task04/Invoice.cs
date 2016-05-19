e756e191affab1956e81340b075a49dbfb1439bf
zvanya <zhakin.v@gmail.com>
2016-05-19 11:12:06 +0300

    002 Task04 Complete

--- 002_Classes/Task04/Invoice.cs
+++ 002_Classes/Task04/Invoice.cs
@@ -12,11 +12,11 @@ namespace Task04
     readonly string customer = null; // клиент
     readonly string provider = null; // поставщик
 
-    string article = null; // товар
+    public string Article { get; set; } = null; // товар
     int quantity = 0; // количество товара
 
     double tax = 0.1;
-    double articleCost = 50;
+    double articleCost = 0;
 
     public Invoice(int account, string customer, string provider)
     {
@@ -29,6 +29,29 @@ namespace Task04
     public string Customer { get { return customer; } }
     public string Provider { get { return provider; } }
 
+    public int Quantity
+    {
+      get { return quantity; }
+      set { quantity = Math.Abs(value); }
+    }
+    public double ArticleCost
+    {
+      get { return articleCost; }
+      set { articleCost = Math.Abs(value); }
+    }
+    public double Tax
+    {
+      get { return tax; }
+      set { tax = value == 0 ? 1 : Math.Abs(value); }
+    }
 
+    public double CostWithTaxCalculate()
+    {
+      return CostCalculate() + Tax * CostCalculate();
+    }
+    public double CostCalculate()
+    {
+      return Quantity * ArticleCost;
+    }
   }
 }
--- 002_Classes/Task04/Program.cs
+++ 002_Classes/Task04/Program.cs
@@ -10,6 +10,19 @@ namespace Task04
   {
     static void Main(string[] args)
     {
+      Invoice inv = new Invoice(1, "Клиент1", "Поставщик1");
+
+      inv.Article = "Товар1";
+      inv.ArticleCost = 100;
+      inv.Tax = 0.2;
+      inv.Quantity = 5;
+
+      Console.WriteLine("Счет №{0}\nКлиент: {1}\nПоставщик: {2}\nТовар: {3}\nЦена: {4}\nНДС: {5}%\nКоличество: {6}", inv.Account, inv.Customer, inv.Provider, inv.Article, inv.ArticleCost, inv.Tax * 100, inv.Quantity);
+      Console.WriteLine(new string('-',50));
+      Console.WriteLine("Цена без НДС: {0}", inv.CostCalculate());
+      Console.WriteLine("Цена с НДС: {0}", inv.CostWithTaxCalculate());
+
+      Console.ReadKey();
     }
   }
 }
