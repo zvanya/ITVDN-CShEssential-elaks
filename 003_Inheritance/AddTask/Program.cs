8ab119e41e059a44964f792915e22bd66f6705b0
zvanya <zhakin.v@gmail.com>
2016-05-20 12:38:48 +0300

    004 AddTask

--- 003_Inheritance/AddTask/Program.cs
+++ 003_Inheritance/AddTask/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace AddTask
 {
+  class Printer
+  {
+    public virtual void Print(string value)
+    {
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.WriteLine(value);
+    }
+  }
+
+  class HP : Printer
+  {
+    public override void Print(string value)
+    {
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.WriteLine(value);
+    }
+  }
+  class Canon : Printer
+  {
+    public override void Print(string value)
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine(value);
+    }
+  }
+
   class Program
   {
     static void Main(string[] args)
     {
+
+      Printer c = new Canon();
+
+      c.Print("canon");
+
+      Printer p = c as Printer;
+
+      p.Print("printer");
+
+      Printer hp = new HP();
+
+      hp.Print("HP");
+
+      Console.ReadKey();
     }
   }
 }
