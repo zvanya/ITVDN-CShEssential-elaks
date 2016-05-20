1c10f3a823b0042f8f0c6310ca2fcf6861a8be4f
zvanya <zhakin.v@gmail.com>
2016-05-20 17:29:13 +0300

    003 Task03 v1

---
+++ 003_Inheritance/Task03/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+  class Vehicle // Транспортное средство
+  {
+    protected double price = 0;
+    protected double speed = 0;
+    protected int yearCreation = 0;
+
+    public Vehicle(double price, double speed, int yearCreation)
+    {
+      this.price = price;
+      this.speed = speed;
+      this.yearCreation = yearCreation;
+    }
+
+    public double X { get; set; }
+    public double Y { get; set; }
+
+
+  }
+}
