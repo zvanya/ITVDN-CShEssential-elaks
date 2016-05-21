63c25cfe5cc1810b016f4a8aced39926d373b169
zvanya <zhakin.v@gmail.com>
2016-05-21 12:49:45 +0300

    003 Task04 complete

---
+++ 003_Inheritance/Task04/DocumentWorker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+  class DocumentWorker
+  {
+    public virtual void Open()
+    {
+      Console.WriteLine("Документ открыт");
+    }
+    public virtual void Edit()
+    {
+      Console.WriteLine("Редактирование документа доступно в версии Про");
+    }
+    public virtual void Save()
+    {
+      Console.WriteLine("Сохранение документа доступно в версии Про");
+    }
+  }
+
+  class ProDocumentWorker : DocumentWorker
+  {
+    public override void Edit()
+    {
+      Console.WriteLine("Документ отредактирован");
+    }
+    public override void Save()
+    {
+      Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+    }
+  }
+
+  class ExpertDocumentWorker : ProDocumentWorker
+  {
+    public override void Save()
+    {
+      Console.WriteLine("Документ сохранен в новом формате.");
+    }
+  }
+}
---
+++ 003_Inheritance/Task04/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int key = 0;
+      const int keyPro = 111;
+      const int keyExpert = 222;
+
+      DocumentWorker dw = null;
+
+      Console.Write("Введите ключ продукта: ");
+
+      int.TryParse(Console.ReadLine(), out key);
+      
+      //try
+      //{
+      //  key = int.Parse(Console.ReadLine());
+      //}
+      //catch (Exception)
+      //{
+      //  key = 0;
+      //}
+
+      switch (key)
+      {
+        case keyPro:
+          dw = new ProDocumentWorker();
+          break;
+        case keyExpert:
+          dw = new ExpertDocumentWorker();
+          break;
+        default:
+          dw = new DocumentWorker();
+          break;
+      }
+
+      dw.Open();
+      dw.Edit();
+      dw.Save();
+
+      Console.ReadKey();
+    }
+  }
+}
