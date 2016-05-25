0ff34c124f50d50a8023c8baaf1313c4d99c3fa9
zvanya <zhakin.v@gmail.com>
2016-05-25 09:50:04 +0300

    004_Abstraction solution added

---
+++ 004_Abstraction/AddTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
