faa849c5e7df5dd26f66be696555085dab296b07
zvanya <zhakin.v@gmail.com>
2016-05-30 14:07:47 +0300

    004_Abstraction Task04 complete

---
+++ 004_Abstraction/Task03/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+  class Player : IPlayable, IRecordable
+  {
+    public void Pause()
+    {
+      Console.WriteLine("Pause");
+    }
+
+    public void Play()
+    {
+      Console.WriteLine("Play");
+    }
+
+    public void Record()
+    {
+      Console.WriteLine("Record");
+    }
+
+    public void Stop()
+    {
+      Console.WriteLine("Stop");
+    }
+  }
+}
---
+++ 004_Abstraction/Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Player p = new Player();
+
+      p.Play();
+      p.Stop();
+      p.Record();
+      p.Pause();
+      p.Stop();
+
+      Console.ReadKey();
+    }
+  }
+}
