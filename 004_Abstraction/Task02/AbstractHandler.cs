86a9eb4bdb975afe38852cdd0092f20abbbdac9f
zvanya <zhakin.v@gmail.com>
2016-05-30 12:34:33 +0300

    006_Static AddTask added

--- 004_Abstraction/Task02/AbstractHandler.cs
+++ 004_Abstraction/Task02/AbstractHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Task02
 {
-  class AbstractHandler
+  abstract class AbstractHandler
   {
     public void Open()
     { }
---
+++ 006_Static/AddTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+  class Program
+  {
+    static class Calculator
+    {
+      public static float Plus(float x1, float x2)
+      {
+        return x1 + x2;
+      }
+      public static float Dev(float x1, float x2)
+      {
+        return x1 / x2;
+      }
+
+    }
+    static void Main(string[] args)
+    {
+
+      Console.WriteLine(Calculator.Plus(15, 5));
+      Console.ReadKey();
+    }
+  }
+}
