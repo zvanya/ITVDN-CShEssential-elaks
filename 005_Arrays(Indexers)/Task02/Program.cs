dcd41935e6104de6edcfe88f37389f4872e1d521
zvanya <zhakin.v@gmail.com>
2016-05-30 16:03:31 +0300

    005_Arrays Task02 added

---
+++ 005_Arrays(Indexers)/Task02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] m = new int[new Random().Next(5,21)];
+
+      Random r = new Random();
+
+      for (int i = 0; i < m.Length; i++)
+        m[i] = r.Next(0, 101);
+
+      for (int i = 0; i < m.Length; i++)
+        Console.Write("{0} ", m[i]);
+
+      Console.WriteLine();
+
+      Console.WriteLine("Summ = {0}", m.Sum());
+      Console.WriteLine("Max = {0}", m.Max());
+
+      Console.ReadKey();
+    }
+  }
+}
