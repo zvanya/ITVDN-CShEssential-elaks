9653296caf2271c3b1b7b2b5ec2c8f0780902316
zvanya <zhakin.v@gmail.com>
2016-05-31 18:02:21 +0300

    005_Arrays Task03 complete

---
+++ 005_Arrays(Indexers)/Task03/MyMatrix.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+  class MyMatrix
+  {
+    int row = 0;
+    int col = 0;
+    float[,] m;
+
+    public MyMatrix(int row, int col)
+    {
+      this.row = row;
+      this.col = col;
+      m = new float[row, col];
+
+      Random r = new Random();
+      for (int i = 0; i < m.GetLength(0); i++)
+        for (int j = 0; j < m.GetLength(1); j++)
+          m[i, j] = r.Next(0, 101);
+    }
+
+    public static void Resize(int newRow, int newCol, ref float[,] m) // Поменять ref на out
+    {
+      if (m.GetLength(0) < 1 || m.GetLength(1) < 1)
+      {
+        Console.WriteLine("Неверно задан массив на входе.");
+        return;
+      }
+      if (newRow < 1 || newCol < 1 || newRow > m.GetLength(0) || newCol > m.GetLength(1))
+      {
+        Console.WriteLine("Неверно заданы размеры нового массива.");
+        return;
+      }
+
+      float[,] newM = new float[m.GetLength(0), m.GetLength(1)];
+
+      for (int i = 0; i < newRow; i++)
+      {
+        for (int j = 0; j < newCol; j++)
+        {
+          newM[i, j] = m[i, j];
+        }
+      }
+
+      m = new float[newRow, newCol];
+
+      for (int i = 0; i < newRow; i++)
+      {
+        for (int j = 0; j < newCol; j++)
+        {
+          m[i, j] = newM[i, j];
+        }
+      }
+
+    }
+    
+    public void Resize(int newRow, int newCol)
+    {
+      if (m.GetLength(0) < 1 || m.GetLength(1) < 1)
+      {
+        Console.WriteLine("Неверно задан массив на входе.");
+        return;
+      }
+      if (newRow < 1 || newCol < 1 || newRow > m.GetLength(0) || newCol > m.GetLength(1))
+      {
+        Console.WriteLine("Неверно заданы размеры нового массива.");
+        return;
+      }
+
+      float[,] newM = new float[newRow, newCol];
+
+      for (int i = 0; i < newRow; i++)
+      {
+        for (int j = 0; j < newCol; j++)
+        {
+          newM[i, j] = this.m[i, j];
+        }
+      }
+
+      m = newM;
+    }
+
+    public void Show()
+    {
+      for (int i = 0; i < m.GetLength(0); i++)
+      {
+        for (int j = 0; j < m.GetLength(1); j++)
+          Console.Write("{0} ", m[i, j]);
+        Console.WriteLine();
+      }
+    }
+
+    public float this[int row, int col] //Индексатор для матрицы
+    {
+      get
+      {
+        if (row >= 0 && row < m.GetLength(0) && col >= 0 && col < m.GetLength(1))
+          return m[row,col];
+        Console.WriteLine("Попытка обращения за пределы массива.");
+        return 0;
+      }
+
+      set
+      {
+        if (row >= 0 && row < m.GetLength(0) && col >= 0 && col < m.GetLength(1))
+          m[row,col] = value;
+        else
+          Console.WriteLine("Попытка записи за пределами массива.");
+      }
+    }
+  }
+}
---
+++ 005_Arrays(Indexers)/Task03/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MyMatrix m = new MyMatrix(10, 10);
+
+      Console.WriteLine("Массив MyMatrix.m: ");
+      m.Show();
+
+      m.Resize(5, 5);
+      Console.WriteLine();
+      Console.WriteLine("Новый массив MyMatrix.m: ");
+      m.Show();
+
+      float[,] mm = new float[5, 5];
+      Random r = new Random();
+      for (int i = 0; i < mm.GetLength(0); i++)
+        for (int j = 0; j < mm.GetLength(1); j++)
+          mm[i, j] = r.Next(0, 101);
+
+      Console.WriteLine();
+      Console.WriteLine("Массив mm: ");
+      for (int i = 0; i < mm.GetLength(0); i++)
+      {
+        for (int j = 0; j < mm.GetLength(1); j++)
+          Console.Write("{0} ", mm[i, j]);
+        Console.WriteLine();
+      }
+
+      Console.WriteLine();
+      Console.WriteLine("Новый массив mm, полученный статическим методом класса MyMatrix: ");
+      MyMatrix.Resize(3, 2, ref mm);
+      for (int i = 0; i < mm.GetLength(0); i++)
+      {
+        for (int j = 0; j < mm.GetLength(1); j++)
+          Console.Write("{0} ", mm[i, j]);
+        Console.WriteLine();
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
