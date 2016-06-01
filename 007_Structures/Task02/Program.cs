bb24bb1839368e7e879e34ae9fda51cdafc57da3
zvanya <zhakin.v@gmail.com>
2016-06-01 12:25:11 +0300

    007_Structures Task02 added

---
+++ 007_Structures/Task02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+  struct Train
+  {
+    public string TrainNumber { set; get; }
+    public string Destination { set; get; }
+    public string DepartureTime { set; get; }
+
+    public void ShowInfo()
+    {
+      Console.Write("Номер поезда:");
+      Console.WriteLine(TrainNumber);
+      Console.Write("Пункт назначения:");
+      Console.WriteLine(Destination);
+      Console.Write("Время отправления:");
+      Console.WriteLine(DepartureTime);
+      Console.WriteLine(new string('-', 50));
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int trainCount = 3;
+
+      Train[] t = new Train[trainCount];
+
+      for (int i = 0; i < trainCount; i++)
+      {
+        Console.WriteLine("Введите информацию о {0}-м поезде:", i+1);
+        Console.Write("Номер поезда:");
+        t[i].TrainNumber = Console.ReadLine();
+        Console.Write("Пункт назначения:");
+        t[i].Destination = Console.ReadLine();
+        Console.Write("Время отправления:");
+        t[i].DepartureTime = Console.ReadLine();
+
+        Console.WriteLine(new string('-',50));
+      }
+
+      for (int i = 0; i < trainCount; i++)
+        t[i].ShowInfo();
+
+      Console.ReadKey();
+    }
+  }
+}
