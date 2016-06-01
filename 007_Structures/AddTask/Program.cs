64dcf103ae98339d5970b2988d0a6f3a7b6bbc5a
zvanya <zhakin.v@gmail.com>
2016-06-01 12:06:54 +0300

    005_Arrays Task04 added 007_Structures AddTask complete

---
+++ 007_Structures/AddTask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+  struct Notebook
+  {
+    string model;
+    string manufactor;
+    float price;
+
+    public Notebook(string model, string manufactor, float price)
+    {
+      this.model = model;
+      this.manufactor = manufactor;
+      this.price = price;
+    }
+
+    public void ShowInfo()
+    {
+      Console.WriteLine("Ноутбук: {0}", manufactor);
+      Console.WriteLine("Модель: {0}", model);
+      Console.WriteLine("Цена: {0}", price);
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Notebook n = new Notebook("A33J5", "Asus", 20000);
+      n.ShowInfo();
+
+      Console.ReadKey();
+    }
+  }
+}
