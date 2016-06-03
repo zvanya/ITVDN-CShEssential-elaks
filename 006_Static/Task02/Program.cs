320c44e4b2020d62be2976c0b37c8f1be8556cd1
zvanya <zhakin.v@gmail.com>
2016-06-03 12:56:27 +0300

    008_Structures AddTask

--- 006_Static/Task02/Program.cs
+++ 006_Static/Task02/Program.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace Task02
 {
+  class FindAndReplaceManager
+  {
+    static public void FindNext(string str)
+    {
+      Console.WriteLine("Поиск строки : " + str);
+    }
+
+    static public void FindAndReplace(string str, string newStr)
+    {
+
+    }
+  }
+
   class Program
   {
     static void Main(string[] args)
---
+++ 008_Structures/AddTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите дату своего рождения: ");
+
+      DateTime birthday = DateTime.Parse(Console.ReadLine());
+      DateTime now = DateTime.Now;
+
+      DateTime nextBirthdayYear = now.AddYears(1);
+      string nextBirthday = string.Format ("{0}.{1}.{2}", birthday.Day, birthday.Month, nextBirthdayYear.Year);
+
+      Console.WriteLine("Сегодня: {0}", now);
+
+      Console.WriteLine("Дата рождения: {0}", birthday.ToString());
+      Console.WriteLine("Следующий день рождения: {0}", DateTime.Parse(nextBirthday));
+
+      Console.WriteLine("До следующего дня рождения: \n дней: {0}", ((TimeSpan)(DateTime.Parse(nextBirthday) - now)).Days);
+
+      Console.ReadKey();
+    }
+  }
+}
