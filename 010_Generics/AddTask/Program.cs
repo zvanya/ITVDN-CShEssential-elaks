78e24972218dd77e9cf8fae4d6b8971157de82df
zvanya <zhakin.v@gmail.com>
2016-06-10 12:02:56 +0300

    010_Generic AddTask

---
+++ 010_Generics/AddTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+  class MyClass<T> where T : new()
+  {
+    static public T FactoryMethod()
+    {
+      return new T();
+    }
+  }
+
+  class A
+  {
+    public void Print()
+    {
+      Console.WriteLine("ClassA");
+    }
+  }
+  class B
+  {
+    public void Print()
+    {
+      Console.WriteLine("ClassB");
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      A a = MyClass<A>.FactoryMethod();
+      a.Print();
+
+      B b = MyClass<B>.FactoryMethod();
+      b.Print();
+
+      Console.ReadKey();      
+    }
+  }
+}
