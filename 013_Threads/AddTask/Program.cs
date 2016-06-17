c63936a5f6f8df00be93bb101827444422f89497
zvanya <zhakin.v@gmail.com>
2016-06-17 12:06:36 +0300

    013_Threads AddTask v1

---
+++ 013_Threads/AddTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AddTask
+{
+    class Program
+    {
+        static Thread t = null;
+        static string exit = null;
+
+        static void myMethod()
+        {
+            t = new Thread(myMethod);
+            Console.WriteLine("myMethod");
+
+            string exit = Console.ReadLine();
+
+            if (exit == "exit") Thread.CurrentThread.Abort();
+
+            t.Start();
+        }
+
+        static void Main(string[] args)
+        {
+            t = new Thread(myMethod);
+            t.Start();
+        }
+    }
+}
