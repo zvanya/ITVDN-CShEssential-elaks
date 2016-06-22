98914067c0f63e0a151599c3abf76a3f43fa03cf
zvanya <zhakin.v@gmail.com>
2016-06-22 12:40:24 +0300

    014_Collections AddTask

---
+++ 014_Collections/AddTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    class Program
+    {
+        static myCollection GetCollection(int?[] a)
+        {
+            return new myCollection(a);
+        }
+
+        static void Main(string[] args)
+        {
+            int?[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            myCollection mC = new myCollection(arr);
+
+            foreach (int? item in mC)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 014_Collections/AddTask/myCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    class myCollection : IEnumerable
+    {
+        int?[] arr = null;
+        int position = -1;
+
+        public int? this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < arr.Length)
+                    return arr[index];
+                else
+                    return null;
+            }
+            set
+            {
+                if (index >= 0 && index < arr.Length)
+                    arr[index] = value;
+                else
+                    Console.WriteLine("Попытка записи за пределами массива.");
+            }
+        }
+
+        public myCollection(int?[] a)
+        {
+            int k = 0; //количество четных элементов
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] % 2 == 0) k++;
+
+            arr = new int?[k];
+
+            k = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    k++;
+                    arr[k - 1] = a[i];
+                }
+            }
+
+        }
+
+        void Reset()
+        {
+            position = -1;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < arr.Length - 1)
+                {
+                    position++;
+                    yield return arr[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;  // Выход из цикла.       
+                }
+            }
+
+        }
+    }
+}
