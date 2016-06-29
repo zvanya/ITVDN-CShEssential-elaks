789a072316c18f8aa5f47b80a773b8b7c741f0b9
zvanya <zhakin.v@gmail.com>
2016-06-29 12:28:04 +0300

    015,016

---
+++ 015_Exceptions/Task02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 0, 5, 1, 4, 2, 3, 3, 0, 4, 1 };
+
+            List<int> l = new List<int> { 0, 1, 0, 0, 2, 3, 4, 5 };
+
+            IEnumerable<int> s = new List<int>();
+            s = l.Distinct();
+
+            
+
+            foreach (int item in s)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 016_Operators/AddTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Point(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"new Point({X}, {Y}, {Z})");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 2, 3);
+            Point p2 = new Point(3, 2, 1);
+
+            Console.WriteLine(p1 + p2);
+
+            Console.ReadKey();
+        }
+    }
+}
